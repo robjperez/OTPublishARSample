e02606be46c1410f57f3d7fd5ccd464b184e6684
Roberto Perez Cubero <robj.perez@gmail.com>
2018-04-13 17:23:29 +0200

    Initial commit

--- OTPublishARSample/ViewController.swift
+++ OTPublishARSample/ViewController.swift
@@ -9,26 +9,34 @@
 import UIKit
 import SceneKit
 import ARKit
+import OpenTok
+
+let kApiKey = ""
+let kToken = ""
+let kSessionId = ""
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+        
+    var sessionDelegate: ViewControllerSessionDelegate?
+    var otSession: OTSession?
+    var otPublisher: OTPublisher?
+    var scnViewCapturer: SCNViewVideoCapture?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.scene = scene                  
         
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
+        sessionDelegate = ViewControllerSessionDelegate(self)
+        otSession = OTSession(apiKey: kApiKey, sessionId: kSessionId, delegate: sessionDelegate)
+        scnViewCapturer = SCNViewVideoCapture(sceneView: sceneView)
+    }        
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -40,6 +48,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        otSession?.connect(withToken: kToken, error: nil)
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
@@ -47,13 +59,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    func sessionConnected() {
+        let pubSettings = OTPublisherSettings()
+        pubSettings.videoCapture = scnViewCapturer
+        otPublisher = OTPublisher(delegate: self, settings: pubSettings)
+        otSession!.publish(otPublisher!, error: nil)
     }
+    
 
     // MARK: - ARSCNViewDelegate
-    
 /*
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
@@ -65,16 +79,48 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    }
+}
+
+extension ViewController: OTPublisherDelegate {
+    func publisher(_ publisher: OTPublisherKit, didFailWithError error: OTError) {
+        print("Publisher error: \(error)")
+    }
+}
+
+class ViewControllerSessionDelegate : NSObject, OTSessionDelegate {
+    let parent: ViewController
+    
+    init(_ parent: ViewController) {
+        self.parent = parent
+    }
+    
+    func session(_ session: OTSession, didFailWithError error: OTError) {
+        print("Session Fail")
+    }
+    
+    func session(_ session: OTSession, streamCreated stream: OTStream) {
+        print("Stream Created")
+    }
+    
+    func session(_ session: OTSession, streamDestroyed stream: OTStream) {
+        print("Stream Destroyed")
+    }
+    
+    func sessionDidConnect(_ session: OTSession) {
+        print("SessionConnected")
+        parent.sessionConnected()
+    }
+    
+    func sessionDidDisconnect(_ session: OTSession) {
+        print("Disconnect")
     }
 }
